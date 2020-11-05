625374e28115222cc49b46e5ce9fb5bddc623ca4
Gregory Caloin <gregchapauli@gmail.com>
2020-11-05 15:57:46 +0100

    Suppression des tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:leadeetuto/main.dart';
 
-void main() {
+/* void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
@@ -25,6 +25,6 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget); */
   });
 }
