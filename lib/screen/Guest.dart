a79c6c837c3a784d749e06943f88e275ee1c7882
Gregory Caloin <gregchapauli@gmail.com>
2020-11-12 18:09:38 +0100

    Connexion du projet à Firestore

--- lib/screen/Guest.dart
+++ lib/screen/Guest.dart
@@ -4,6 +4,7 @@ import 'package:leadeetuto/screen/dashboard/Home.dart';
 import 'package:leadeetuto/screen/guest/Auth.dart';
 import 'package:leadeetuto/screen/guest/Password.dart';
 import 'package:leadeetuto/screen/guest/Term.dart';
+import 'package:leadeetuto/screen/services/CommonService.dart';
 import 'package:leadeetuto/screen/services/UserService.dart';
 
 class GuestScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class GuestScreen extends StatefulWidget {
 }
 
 class _GuestScreenState extends State<GuestScreen> {
+  CommonService _commonService = CommonService();
   UserService _userService = UserService();
 
   List<Widget> _widgets = [];
@@ -23,50 +25,63 @@ class _GuestScreenState extends State<GuestScreen> {
   void initState() {
     super.initState();
 
-    _widgets.addAll([
-      AuthScreen(
-        onChangedStep: (index, value) => setState(() {
-          _indexSelected = index;
-          _email = value;
-        }),
-      ),
-      TermScreen(
-        onChangedStep: (index) => setState(() => _indexSelected = index),
-      ),
-      PasswordScreen(
-        onChangedStep: (index, value) => setState(() {
-          if (index != null) {
-            _indexSelected = index;
-          }
+    _commonService.terms.then(
+      (terms) => setState(
+        () => _widgets.addAll([
+          AuthScreen(
+            onChangedStep: (index, value) => setState(() {
+              _indexSelected = index;
+              _email = value;
+            }),
+          ),
+          TermScreen(
+            terms: terms,
+            onChangedStep: (index) => setState(() => _indexSelected = index),
+          ),
+          PasswordScreen(
+            onChangedStep: (index, value) => setState(() {
+              if (index != null) {
+                _indexSelected = index;
+              }
 
-          if (value != null) {
-            _userService
-                .auth(UserModel(
-              email: _email,
-              password: value,
-            ))
-                .then(
-              (value) {
-                if (value.uid != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
-                    ),
-                  );
-                }
-              },
-            );
-          }
-        }),
+              if (value != null) {
+                _userService
+                    .auth(UserModel(
+                  email: _email,
+                  password: value,
+                ))
+                    .then(
+                  (value) {
+                    if (value.uid != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ),
+                      );
+                    }
+                  },
+                );
+              }
+            }),
+          ),
+        ]),
       ),
-    ]);
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: _widgets.elementAt(_indexSelected),
+      child: _widgets.length == 0
+          ? SafeArea(
+              child: Scaffold(
+                body: Center(
+                  child: Text('Loading...'),
+                ),
+              ),
+            )
+          : _widgets.elementAt(_indexSelected),
     );
   }
 }
--- lib/screen/guest/Term.dart
+++ lib/screen/guest/Term.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class TermScreen extends StatefulWidget {
   final Function(int) onChangedStep;
+  final String terms;
 
   const TermScreen({
     Key key,
     this.onChangedStep,
+    this.terms,
   }) : super(key: key);
 
   @override
@@ -66,7 +68,7 @@ class _TermScreenState extends State<TermScreen> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(
-                        "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même.\n L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).\nOn sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même.\n L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).\nOn sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même.\n L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).\n",
+                        widget.terms,
                       ),
                     ],
                   ),
